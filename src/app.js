53a0957367610d19a5ed7ecdaeb63a2b46f59b6f
Luiz Assumpção <luizrafael.apneto@gmail.com>
2026-09-04 01:23:15 -0300

    Split app and server and add test dependencies

--- src/app.js
+++ src/app.js
@@ -4,13 +4,10 @@ import urlRoutes from './routes/url.routes.js';
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 
 app.use('/', urlRoutes);
 app.use('/auth', authRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
